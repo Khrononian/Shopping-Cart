b6f164e79511d1c5f092cfa809783b69d4a70adc
Khrononian <Alv.Barnes@gmail.com>
2022-09-28 23:56:13 -0400

    Used components to post data and removed a hook

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Laptops from './Laptops'
+import Mouse from './Mouse'
 import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
@@ -11,14 +13,6 @@ const Products = (props) => {
   const cartData = []
   const products = [];
 
-  // USE CART DATA TO POST THE PRODUCTS IN CART INTO THE CART PAGE FOR DISPLAY. 
-  // USE SET TO ADD NEW ONES, MAP THE ARRAY OF OBJECTS TO POST THE DATA (CODE GOES IN CART.JS)
-  // SAVE THE DATA
-
-  // ARRAY DOESNT SAVE THE OTHER PRODUCT FOR SOME REASON
-  useEffect(() => {
-
-  }, [])
   useEffect(() => {
     const savedProducts = JSON.parse(window.localStorage.getItem('products'))
     const savedCart = JSON.parse(window.localStorage.getItem('cartData'))
@@ -36,25 +30,27 @@ const Products = (props) => {
       // setCartInfo(prevCart => prevCart.concat(savedCart[data]))
       // window.localStorage.setItem('cartInfo', JSON.stringify(cartInfo))
     }
+    // window.localStorage.setItem('laptops', JSON.stringify(props.laptops))
+    // window.localStorage.setItem('mouse', JSON.stringify(props.mouse))
     console.log('Intro', products, window.localStorage)
     // products.concat(savedProducts)
     
   }, [products, cartData])
   
   // window.localStorage.clear()
-
+  console.log('LAPPY', props)
   const addToCart = event => {
     const findLaptop = props.laptops.find(item => item.title === event.nativeEvent.path[2].firstChild.innerText)
     const findMouse = props.mouse.find(item => item.title === event.nativeEvent.path[2].firstChild.innerText)
     
     if (findLaptop) {
-      setCartInfo(prevCart => prevCart.concat(findLaptop))
+      setCartInfo(cartInfo.concat(findLaptop))
       
       getItemType(findLaptop)
     }
 
     if (findMouse) {
-      setCartInfo(prevCart => prevCart.concat(findMouse))
+      setCartInfo(cartInfo.concat(findMouse))
 
       getItemType(findMouse)
     }
@@ -68,13 +64,13 @@ const Products = (props) => {
   const getItemType = (type) => {
     JSON.parse(window.localStorage.getItem('products'))
     JSON.parse(window.localStorage.getItem('cartData'))
-
+    console.log('ARRAY', cartData, products, cartInfo)
     if (!window.localStorage.total) {
       window.localStorage.setItem('total', Number(type.price))
       window.localStorage.setItem('Total', Number(type.price) + Number(window.localStorage.total))
       window.localStorage.setItem('cartNum', Number(cartNum) + 1)
       setCartNum(Number(cartNum) + 1)
-
+      type.quantity = 1
       products.push(type.title)
       cartData.push(type)
     } else if (cartData.find(current => current.title === type.title) ) {
@@ -95,18 +91,20 @@ const Products = (props) => {
       products.push(type.title)
       cartData.push(type)
       console.log('ME ME', window.localStorage)
+      type.quantity = 1
       setCartNum(Number(cartNum) + 1)
     }
   }
 
   return (
-    <div>
+    <div id='main'>
+      
       <Nav cartNum={cartNum} products={JSON.parse(window.localStorage.getItem('products'))} />
       <header> 
         <h1>All Products</h1>
       </header>
-      <div className='grid'>
-        {props.laptops.map((laptop, index) => {
+      {/* <div className='grid'> */}
+        {/* {props.laptops.map((laptop, index) => {
           return (
             <div className='card' key={index}>
               <div >
@@ -120,9 +118,10 @@ const Products = (props) => {
               </div>
             </div>
           )
-        })}
-
-        {props.mouse.map((current, index) => {
+        })} */}
+        <Laptops laptops={props.laptops} data={addToCart}/>
+        <Mouse mouse={props.mouse} data={addToCart}/>
+        {/* {props.mouse.map((current, index) => {
           return (
             <div className='card' key={index}>
               <div>
@@ -136,9 +135,9 @@ const Products = (props) => {
               </div>
             </div>
           )
-        })}
+        })} */}
       </div>
-    </div>
+    // </div>
   )
 }
 
